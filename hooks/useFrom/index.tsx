122a42371c63427fb5bf4985ac38ee084d7a8d51
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2021-06-29 21:34:24 +0100

    feat: add useForm hook

---
+++ hooks/useFrom/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type NativeElement = React.ChangeEvent<HTMLInputElement & HTMLTextAreaElement>;
+
+export function useForm<T>(
+    initialValues: T,
+    onSubmit?: (data: T) => void
+): [
+    T,
+    (event: NativeElement) => void,
+    (event: React.SyntheticEvent) => void
+] {
+    const [formData, setFormData] = React.useState<T>(initialValues);
+
+    const handleInputChange = (event: NativeElement) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        if (onSubmit) onSubmit(formData);
+    };
+
+    return [formData, handleInputChange, handleSubmit];
+}
